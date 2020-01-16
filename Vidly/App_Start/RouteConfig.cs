b0eb59669098f56ac3e75700df0ab9b9e8ff57b9
dzonib <nikola.bosnjak@wayseven.com>
2020-01-16 08:07:41 +0100

    Initial commit

---
+++ Vidly/App_Start/RouteConfig.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Vidly
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            // A new way of creating custom routes. You enable it here and do stuff in controller
+            routes.MapMvcAttributeRoutes();
+
+
+            //old way of creating custom routes
+            //routes.MapRoute(
+            //   "MoviesByReleaseDate",
+            //  "movies/released/{year}/{month}",
+            //  new
+            //  {
+            //       controller = "Movies",
+            //      action = "ByReleaseDate"
+            //   },
+            //   new { year = @"\d{4}", month = @"\d{2}" }
+            //   );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
